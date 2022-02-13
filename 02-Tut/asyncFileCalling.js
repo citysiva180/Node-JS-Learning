e30015160de811b71b3065c170cf9f44c61dec12
citysiva180@gmail.com <citysiva180@gmail.com>
2022-02-14 00:40:33 +0530

    completed async file call!

--- 02-Tut/asyncFileCalling.js
+++ 02-Tut/asyncFileCalling.js
@@ -1,4 +1,5 @@
 const fsPromises = require("fs").promises;
+const { deepStrictEqual } = require("assert");
 const path = require("path");
 
 //best way to read and write files in Node.js
@@ -9,6 +10,20 @@ const fileOps = async () => {
       "utf8"
     );
     console.log(data);
+    await fsPromises.writeFile(path.join(__dirname, "promiseWrite.txt"), data);
+    await fsPromises.appendFile(
+      path.join(__dirname, "promiseWrite.txt"),
+      "Nice to meet you appeneded text!"
+    );
+    await fsPromises.rename(
+      path.join(__dirname, "promiseWrite.txt"),
+      path.join(__dirname, "promiseComplete.txt")
+    );
+    const newData = await fsPromises.readFile(
+      path.join(__dirname, "promiseComplete.txt"),
+      "utf-8"
+    );
+    console.log(newData);
   } catch (err) {
     console.log(err);
   }
