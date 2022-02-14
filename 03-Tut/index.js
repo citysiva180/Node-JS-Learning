d7d2bf1a7c52262df6754774f282fb6b4152e71f
citysiva180@gmail.com <citysiva180@gmail.com>
2022-02-14 06:57:19 +0530

    nodemon , package.json script & other details

--- 03-Tut/index.js
+++ 03-Tut/index.js
@@ -1,1 +1,10 @@
-console.log("testing");
+const { format } = require("date-fns");
+const { v4: uuid } = require("uuid");
+
+//Date time formatting
+console.log(format(new Date(), "yyyy-MM-dd\tHH:mm:ss"));
+console.log("hello");
+
+//V4UUID
+console.log(uuid());
+console.log(uuid());
